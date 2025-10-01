e17e35bd61d8fcf91ff5e9360bd519827d388970
Udit Kanti De <uditde.com@gmail.com>
2025-10-01 10:17:22 +0530

    sidebar ui complete

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,57 @@
+import type React from "react";
 import "./styles/Sidebar.scss";
 import { FaRegUserCircle } from "react-icons/fa";
 import { GiNinjaHeroicStance } from "react-icons/gi";
 import { GiPowerLightning } from "react-icons/gi";
 import { GiProcessor } from "react-icons/gi";
+import { GoSidebarExpand } from "react-icons/go";
+import { GoSidebarCollapse } from "react-icons/go";
 
-const Sidebar = () => {
+type Sidebar_type = {
+    isCollapsed: boolean;
+    toggleSideBar: () => void;
+};
+
+const Sidebar: React.FC<Sidebar_type> = ({ isCollapsed, toggleSideBar }) => {
     return (
-        <div className="sidebar-container">
-            <img src="logo/lumen-logo-v2.png" alt="lumnen" />
-            <h2>DSA Sheets</h2>
-            <div className="option-list-container">
+        <div className="sidebar-container" onClick={() => {
+            if(isCollapsed) toggleSideBar();
+        }}>
+            <span onClick={toggleSideBar} className="toggle-btn">
+                {!isCollapsed && <GoSidebarExpand />}
+            </span>
+            <img
+                src={`${
+                    isCollapsed
+                        ? "logo/lumen-sign-v2.png"
+                        : "logo/lumen-logo-v2.png"
+                }`}
+                className={isCollapsed ? "collapsed-image" : ""}
+                alt="lumnen"
+            />
+            {!isCollapsed && <h2>DSA Sheets</h2>}
+            <div className={`option-list-container ${isCollapsed && "option-list-container-collapsed"}`}>
                 <span>
                     {" "}
-                    <GiNinjaHeroicStance /> Scratch to Conqueror
+                    <GiNinjaHeroicStance />{" "}
+                    {!isCollapsed && "Scratch to Conqueror"}
                 </span>
                 <span>
                     {" "}
-                    <GiPowerLightning /> Speed Run
+                    <GiPowerLightning /> {!isCollapsed && "Speed Run"}
                 </span>
                 <span>
                     {" "}
-                    <GiProcessor /> Advanced Algo
+                    <GiProcessor /> {!isCollapsed && "Advanced Algo"}
                 </span>
             </div>
-            <div className="account-info-container">
+            <div
+                className={`account-info-container ${
+                    isCollapsed && "account-info-container-collapsed"
+                }`}
+            >
                 <FaRegUserCircle />
-                <span>testuser</span>
+                {!isCollapsed && <span>testuser</span>}
             </div>
         </div>
     );
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import "./styles/Layout.scss";
 import Sidebar from "../components/Sidebar";
+import { useState } from "react";
 
 const Layout = () => {
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const toggleSideBar = () => setIsCollapsed(!isCollapsed);
     return (
         <div className="body-layout">
-            <div className="left-container">
-                <Sidebar />
+            <div className={`left-container ${isCollapsed ? "collapsed" : "not-collapsed"}`}>
+                <Sidebar isCollapsed={isCollapsed} toggleSideBar={toggleSideBar}/>
             </div>
-            <div className="right-container">
+            <div className={`right-container ${isCollapsed ? "expand-right" : "shirnk-right"}`}>
                 <Outlet />
             </div>
         </div>
